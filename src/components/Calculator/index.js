245958efb909bf9293fb70ff7886c0d77f72ed1e
viettu2k <doanviettu2k@gmail.com>
2021-08-02 08:53:43 +0700

    add delete and clear function

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,32 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Screen, Previous, Current, Button } from "./Styled";
 
 export default function Calculator() {
+    const [current, setCurrent] = useState("");
+    const [previous, setPrevious] = useState("");
+    const [operation, setOperation] = useState("");
+
+    const appendValue = (el) => {
+        const value = el.target.getAttribute("data");
+
+        if (value === "." && current.includes(".")) return;
+        setCurrent(current + value);
+        console.log(value);
+    };
+
+    const handleDelete = () => {
+        setCurrent(String(current).slice(0, -1));
+    };
+
+    const handleClear = () => {
+        setCurrent("");
+        setPrevious("");
+        setOperation("");
+    };
+
     return (
         <Container>
             <Screen>
-                <Previous>123</Previous>
-                <Current>123</Current>
+                <Previous>
+                    {previous} {operation}
+                </Previous>
+                <Current>{current}</Current>
             </Screen>
-            <Button gridSpan={2} control>
+            <Button onClick={handleClear} gridSpan={2} control>
                 AC
             </Button>
-            <Button control>DEL</Button>
+            <Button onClick={handleDelete} control>
+                DEL
+            </Button>
             <Button operation>÷</Button>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
+            <Button data={7} onClick={appendValue}>
+                7
+            </Button>
+            <Button data={8} onClick={appendValue}>
+                8
+            </Button>
+            <Button data={9} onClick={appendValue}>
+                9
+            </Button>
             <Button operation>x</Button>
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
+            <Button data={4} onClick={appendValue}>
+                4
+            </Button>
+            <Button data={5} onClick={appendValue}>
+                5
+            </Button>
+            <Button data={6} onClick={appendValue}>
+                6
+            </Button>
             <Button operation>+</Button>
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button> 9</Button>
+            <Button data={1} onClick={appendValue}>
+                1
+            </Button>
+            <Button data={2} onClick={appendValue}>
+                2
+            </Button>
+            <Button data={3} onClick={appendValue}>
+                3
+            </Button>
             <Button operation>-</Button>
-            <Button period>.</Button>
-            <Button>0</Button>
+            <Button data={"."} period onClick={appendValue}>
+                .
+            </Button>
+            <Button data={0} onClick={appendValue}>
+                0
+            </Button>
             <Button gridSpan={2} equals>
                 =
             </Button>
