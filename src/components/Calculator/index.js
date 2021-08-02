a2151f887b3ce4d1644501664ae9798809be2120
viettu2k <doanviettu2k@gmail.com>
2021-08-02 09:37:42 +0700

    add compute funtion and complete project

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -24,6 +24,55 @@ export default function Calculator() {
         setOperation("");
     };
 
+    const chooseOperation = (el) => {
+        if (current === "") return;
+        if (previous !== "") {
+            let value = compute();
+            setPrevious(value);
+        } else {
+            setPrevious(current);
+        }
+
+        setCurrent("");
+        setOperation(el.target.getAttribute("data"));
+    };
+
+    const equals = () => {
+        let value = compute();
+        if (value === undefined || value === null) return;
+
+        setCurrent(value);
+        setPrevious("");
+        setOperation("");
+    };
+
+    const compute = () => {
+        let result;
+        let previousNumber = parseFloat(previous);
+        let currentNumber = parseFloat(current);
+
+        if (isNaN(previousNumber) || isNaN(currentNumber)) return;
+
+        switch (operation) {
+            case "÷":
+                result = previousNumber / currentNumber;
+                break;
+            case "x":
+                result = previousNumber * currentNumber;
+                break;
+            case "+":
+                result = previousNumber + currentNumber;
+                break;
+            case "-":
+                result = previousNumber - currentNumber;
+                break;
+            default:
+                return;
+        }
+
+        return result;
+    };
+
     return (
         <Container>
             <Screen>
@@ -38,44 +87,52 @@ export default function Calculator() {
             <Button onClick={handleDelete} control>
                 DEL
             </Button>
-            <Button operation>÷</Button>
-            <Button data={7} onClick={appendValue}>
+            <Button data={"÷"} onClick={chooseOperation} operation>
+                ÷
+            </Button>
+            <Button data={"7"} onClick={appendValue}>
                 7
             </Button>
-            <Button data={8} onClick={appendValue}>
+            <Button data={"8"} onClick={appendValue}>
                 8
             </Button>
-            <Button data={9} onClick={appendValue}>
+            <Button data={"9"} onClick={appendValue}>
                 9
             </Button>
-            <Button operation>x</Button>
-            <Button data={4} onClick={appendValue}>
+            <Button data={"x"} onClick={chooseOperation} operation>
+                x
+            </Button>
+            <Button data={"4"} onClick={appendValue}>
                 4
             </Button>
-            <Button data={5} onClick={appendValue}>
+            <Button data={"5"} onClick={appendValue}>
                 5
             </Button>
-            <Button data={6} onClick={appendValue}>
+            <Button data={"6"} onClick={appendValue}>
                 6
             </Button>
-            <Button operation>+</Button>
-            <Button data={1} onClick={appendValue}>
+            <Button data={"+"} onClick={chooseOperation} operation>
+                +
+            </Button>
+            <Button data={"1"} onClick={appendValue}>
                 1
             </Button>
-            <Button data={2} onClick={appendValue}>
+            <Button data={"2"} onClick={appendValue}>
                 2
             </Button>
-            <Button data={3} onClick={appendValue}>
+            <Button data={"3"} onClick={appendValue}>
                 3
             </Button>
-            <Button operation>-</Button>
+            <Button data={"-"} onClick={chooseOperation} operation>
+                -
+            </Button>
             <Button data={"."} period onClick={appendValue}>
                 .
             </Button>
-            <Button data={0} onClick={appendValue}>
+            <Button data={"0"} onClick={appendValue}>
                 0
             </Button>
-            <Button gridSpan={2} equals>
+            <Button onClick={equals} gridSpan={2} equals>
                 =
             </Button>
         </Container>
